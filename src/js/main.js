f0319907099ae886e092e04105f0bc33889aaf3f
Poeschl <Poeschl@users.noreply.github.com>
2022-03-14 08:56:03 +0000

    Deploying to gh-pages from main @ 6af3769f53ee614f75f29e7674ecabd2740f3e5d 🚀

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import '@/css/bootstrap-custom.scss';
-import '@mdi/font/scss/materialdesignicons.scss';
-import "bootstrap"
-import {createApp} from 'vue'
-import App from '../App.vue'
-
-const app = createApp(App)
-
-app.mount('#app')
